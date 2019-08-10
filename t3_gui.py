793a4cabf6fc10ba20809ac375b9b6e9f175944c
Jay Kinzie <jaykinzie@gmail.com>
2019-08-09 23:18:29 -0500

    Went back to the older revision of the Gui. Started hooking the gui into the back end. Project display and task display now populate correctly.

--- t3_gui.py
+++ t3_gui.py
@@ -1,36 +1,118 @@
 from t3 import *
-import os
+
 import tkinter as tk
 from tkinter import *
-import json
+
+def poll():
+    # loop speed control
+    # delete_task_button.after(100,poll)
+
+    # tuple with text of all items in Listbox
+    all_project_items = project_list_box.get(0, tk.END)
+    # tuple with indexes of selected items
+    sel_project_index = project_list_box.curselection()
+    # list with text of all selected items
+    sel_project_item = [all_project_items[item] for item in sel_project_index]
+
+    # tuple with text of all items in Listbox
+    all_task_items = task_list_box.get(0, tk.END)
+    # tuple with indexes of selected items
+    sel_task_index = task_list_box.curselection()
+    # list with text of all selected items
+    sel_task_item = [all_task_items[item] for item in sel_task_index]
 
 
-# from t3_gui_functions import *
-# TODO: seperate functions later
 
-#-TEST#
+    task_selection = task_list_box.curselection()
+
+   # print("")
+   # print("project selection : ")
+   # print(sel_project_index)
+   # print(sel_project_item)
+   # print("task selection")
+   # print(task_selection)
+   # print("")
+
+
+    #z = "".join(sel_project_item)
+    # get_project_tasks_method(z)
+    print()
+    print(sel_project_item)
+    print(sel_task_item)
+    print()
+    get_project_tasks_method("".join(sel_project_item))
+
+
+def on_project_list_box_select(self):
+    print("on_project_list_box_select")
+
+    # tuple with text of all items in Listbox
+    all_project_items = project_list_box.get(0, tk.END)
+    # tuple with indexes of selected items
+    sel_project_index = project_list_box.curselection()
+    # list with text of all selected items
+    sel_project_item = [all_project_items[item] for item in sel_project_index]
+
+    get_project_tasks_method("".join(sel_project_item))
+
+
+def on_task_list_box_select(self):
+    print("on_task_list_box_select")
+
+    # tuple with text of all items in Listbox
+    all_task_items = task_list_box.get(0, tk.END)
+    # tuple with indexes of selected items
+    sel_task_index = task_list_box.curselection()
+    # list with text of all selected items
+    sel_task_item = [all_task_items[item] for item in sel_task_index]
+
+
 
 def read_projects_method():
+
+    # initialize jason structure
     inputFile = 'tasks/tasks.json'
     jsonData = open(inputFile).read()
     jsonToPython = json.loads(jsonData)
-
     t3json = jsonToPython['t3']  # pld
-    dumpprojects(t3json)
 
+    # clear the project list box
+    project_list_box.delete(0,'end')
+
+    # iterate through the projects
     for pname in t3json:
-        tasklist = t3json[pname]
-        print("Project " + pname)
-        project_list_box.insert(END, pname)
+        # adds the name of each project to the project list
+        project_list_box.insert(END,pname)
 
 
+def get_project_tasks_method(in_pname):
+
+    if in_pname is None or in_pname is "":
+        return
+
+    # setup json structure
+    inputFile = 'tasks/tasks.json'
+    jsonData = open(inputFile).read()
+    jsonToPython = json.loads(jsonData)
+    t3json = jsonToPython['t3']  # pld
+
+    # iterate through the json to find the project name
+    for pname in t3json:
+        # if the project name matches the name to find
+        if pname == in_pname:
+            # clear the task list
+            task_list_box.delete(0, 'end')
+            # iterate through the task associated with that project and add them to teh list box
+            for task in t3json[pname]:
+                # adds the taskName field as the task in the gui
+                task_list_box.insert(END, task['taskName'])
+
+            task_list_box.select_set(0)
+            task_list_box.event_generate("<<ListboxSelect>>")
 
 
 def new_project_method():
-    project_list_box.insert(END, entry.get())
     print("new project")
-
-    os.system("t3.py --add" + entry.get() + " task1 task2")
 
 
 def edit_project_method():
@@ -38,47 +120,20 @@ def edit_project_method():
 
 
 def delete_project_method():
-    project_list_box.delete(0, END)
     print("delete project")
 
-#TODO:
-"""
-def del_all_projects_method():
-    project_list_box_frame.delete(0, END)
-    print("delete project")
-"""
 
 def new_task_method():
-    task_list_box.insert(END, entry.get())
     print("new task")
 
 
 def edit_task_method():
-
     print("edit task")
 
 
 def delete_task_method():
-    task_list_box.delete(0, END)
     print("delete task")
-"""
-def del_all_tasks_method():
-    task_list_box.delete(0, END)
-    print("delete task")
-"""
 
-#credits to http://effbot.org/tkinterbook
-
-def makeentry(parent, caption, width=None, **options):
-    Label(parent, text=caption).pack(side=LEFT)
-    entry = Entry(parent, **options)  # change var name later (to avoid scope confusion)
-    if width:
-        entry.config(width=width)
-    entry.pack(side=LEFT)
-    return entry
-
-
-# Geometric constants
 
 # window height
 WINDOW_HEIGHT = 800
@@ -89,16 +144,11 @@ BUTTON_HEIGHT = 1
 # button width
 BUTTON_WIDTH = 10
 
-# list of projects
-#project_list = [] #DEL# needed?
-
-# initialize tk (root) as gui and set master to default
+# initialize tk
 gui = tk.Tk()
-master = Tk()
 
 # set the title of the window
-gui.title("GT3: Graphical Todo-list / Time / Tracker")
-
+gui.title("T3: Todo-list / Time / Tracker")
 
 project_button_frame = Frame(gui, bg="white")
 project_button_frame.pack(side=LEFT, fill=BOTH)
@@ -113,100 +163,48 @@ task_button_frame = Frame(gui, background="white")
 task_button_frame.pack(side=LEFT, fill=BOTH)
 
 
-new_project_button = tk.Button(project_button_frame, text="New Project",
-                               borderwidth=2, command=new_project_method,
+
+
+
+
+new_project_button = tk.Button(project_button_frame, text="New Project", borderwidth=2, command=new_project_method,
                                width=BUTTON_WIDTH, height=BUTTON_HEIGHT)
-
-edit_project_button = tk.Button(project_button_frame, text="Edit Project",
-                                borderwidth=2, command=edit_project_method,
+edit_project_button = tk.Button(project_button_frame, text="Edit Project", borderwidth=2, command=edit_project_method,
                                 width=BUTTON_WIDTH, height=BUTTON_HEIGHT)
-
-delete_project_button = tk.Button(project_button_frame, text="Delete Project",
-                                  borderwidth=2, command=delete_project_method,
+delete_project_button = tk.Button(project_button_frame, text="Delete Project", borderwidth=2, command=delete_project_method,
                                   width=BUTTON_WIDTH, height=BUTTON_HEIGHT)
-#TODO:
-"""
-del_all_projects_button = tk.Button(project_button_frame, text="Del. All Projects",
-                                  borderwidth=2, command=del_all_projects_method(),
-                                  width=BUTTON_WIDTH, height=BUTTON_HEIGHT)
-"""
+quit_button = tk.Button(project_button_frame, text="Quit", borderwidth=2, command=quit, width=BUTTON_WIDTH, height=BUTTON_HEIGHT)
 
 new_project_button.pack(side=TOP)
 edit_project_button.pack(side=TOP)
 delete_project_button.pack(side=TOP)
-#del_all_projects_button.pack(side=TOP)
+quit_button.pack(side=TOP)
 
-
-project_list_box: Listbox = Listbox(project_list_box_frame)
+project_list_box = Listbox(project_list_box_frame, selectmode=SINGLE, exportselection=False)
+project_list_box.bind('<<ListboxSelect>>', on_project_list_box_select)
 project_list_box.pack(side=TOP, fill=BOTH, expand=TRUE)
 
-task_list_box = Listbox(task_list_box_frame)
+
+task_list_box = Listbox(task_list_box_frame, selectmode=SINGLE, exportselection=False)
+task_list_box.bind('<<ListboxSelect>>', on_task_list_box_select)
 task_list_box.pack(side=TOP, fill=BOTH, expand=TRUE)
 
 
-
-new_task_button = tk.Button(task_button_frame, text="New Task",
-                            borderwidth=2, command=new_task_method,
-                            width=BUTTON_WIDTH, height=BUTTON_HEIGHT)
-
-edit_task_button = tk.Button(task_button_frame, text="Edit Task",
-                             borderwidth=2, command=edit_task_method,
-                             width=BUTTON_WIDTH, height=BUTTON_HEIGHT)
-
-delete_task_button = tk.Button(task_button_frame, text="Delete Task",
-                               borderwidth=2, command=delete_task_method,
+new_task_button = tk.Button(task_button_frame, text="New Task", borderwidth=2, command=new_task_method,
                                width=BUTTON_WIDTH, height=BUTTON_HEIGHT)
-#TODO:
-"""
-del_all_tasks_button = tk.Button(task_button_frame, text="Del. All Tasks",
-                               borderwidth=2, command=del_all_tasks_method(),
-                               width=BUTTON_WIDTH, height=BUTTON_HEIGHT)
-"""
+edit_task_button = tk.Button(task_button_frame, text="Edit Task", borderwidth=2, command=edit_task_method,
+                                width=BUTTON_WIDTH, height=BUTTON_HEIGHT)
+delete_task_button = tk.Button(task_button_frame, text="Delete Task", borderwidth=2, command=delete_task_method,
+                                  width=BUTTON_WIDTH, height=BUTTON_HEIGHT)
 
 new_task_button.pack(side=TOP)
 edit_task_button.pack(side=TOP)
 delete_task_button.pack(side=TOP)
-#TODO: del_all_tasks_button.pack(side=TOP)
 
 
+# read existing projects
 read_projects_method()
 
-# Entry Widget & String value: gets user's text input (testing)
-entry = Entry(master)
-entry.pack()
 
-entry.focus_set()
-
-"""
-def callback():
-    print(entry.get())
-"""
-# Entry Widget buttons (pb: project button, tb: task button)
-pb = Button(master, text="Projects++", width=10, command=new_project_method)
-pb.pack()
-
-tb = Button(master, text="Tasks++", width=10, command=new_task_method)
-tb.pack()
-# executes the gui (calls tk's main)
+# poll()
 gui.mainloop()
-
-# Entry variables (includes same)
-entry = Entry(master, width=100)
-entry.pack()
-
-text = entry.get()
-
-
-user = makeentry(gui, "User name:", 10)
-password = makeentry(gui, "Password:", 10, show="*")
-content = StringVar()
-entry = Entry(gui, text="caption", textvariable=content)
-
-text = content.get()
-content.set(text)
-
-# Quit button
-quit_button = tk.Button(project_button_frame, text="Quit",
-                        borderwidth=2, command=quit,
-                        width=BUTTON_WIDTH, height=BUTTON_HEIGHT)
-quit_button.pack(side=TOP)
